b0ab8533e3cc7e2a3490138b09496e734dd939ab
Jean Patrick Payette <jp@payette.eu>
2019-04-11 16:01:11 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# gulp
-
-rsync.exe -arouvH --delete --exclude-from '.rsyncignore'  * jppayette.com:/home/docker/volumes/devurandom_io_home/_data/htdocs/wiki/
-
-git add *; git commit -m 'update'; git push
